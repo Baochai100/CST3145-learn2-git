5299af7c07d286c44f62495a2342bb860d428d64
Bao Chai <chaibao2018@gmail.com>
2023-02-05 12:34:17 +0000

    121

--- my-get-routes.js
+++ my-get-routes.js
@@ -137,7 +137,7 @@ app.get(
   "/collections/:collectionName/search/:search",
   function (req, res, next) {
     req.collection
-      .find({ Subject: { $regex: req.params.search, $options: "i" } })
+      .find({ subject: { $regex: req.params.search, $options: "i" } })
       .toArray(function (err, results) {
         if (err) {
           return next(err);
